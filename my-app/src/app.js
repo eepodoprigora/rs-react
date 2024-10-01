8b726e37fb1ae262aede1ed2fb84bc68e810681f
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-01 18:49:32 +0300

    fix instruction

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -7,47 +7,25 @@ export const App = () => {
 	const [steps, setSteps] = useState(data);
 	const [activeIndex, setActiveIndex] = useState(0);
 	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
-	const [firstStep, setFirstStep] = useState(true);
-	const [lastStep, setLastStep] = useState(false);
+
+	const firstStep = activeIndex === 0;
+	const lastStep = activeIndex === steps.length - 1;
 
 	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 
 	function nextStep() {
-		setActiveIndex(activeIndex + 1);
-		if (activeIndex + 1 === steps.length - 1) {
-			setLastStep(true);
-		}
-		if (activeIndex + 1 !== 0) {
-			setFirstStep(false);
-		}
+		setActiveIndex((prev) => prev + 1);
 	}
 	function prevStep() {
-		setActiveIndex(activeIndex - 1);
-		if (activeIndex === 1) {
-			setFirstStep(true);
-		}
+		setActiveIndex((prev) => prev - 1);
 	}
 
 	function startOver() {
-		setLastStep(false);
-		setFirstStep(true);
 		setActiveIndex(0);
 	}
 
 	function stepClick(index) {
 		setActiveIndex(index);
-		if (index === steps.length - 1) {
-			setLastStep(true);
-			setFirstStep(false);
-		}
-		if (index === 0) {
-			setLastStep(false);
-			setFirstStep(true);
-		}
-		if (index !== 0 && index !== steps.length - 1) {
-			setLastStep(false);
-			setFirstStep(false);
-		}
 	}
 
 	return (
@@ -57,22 +35,17 @@ export const App = () => {
 				<div className={styles.steps}>
 					<div key={steps[activeIndex].id} className={styles['steps-content']}>
 						{steps[activeIndex].content}
-						{/* Для получения активного контента использйте steps и activeIndex */}
 					</div>
-
 					<ul className={styles['steps-list']}>
-						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
 						{steps.map((step, i) => (
 							<li
 								onClick={() => stepClick(i)}
 								key={step.id}
 								className={`${styles['steps-item']}${activeIndex === i ? ' ' + styles.active : ''}${activeIndex > i ? ' ' + styles.done : ''}`}
 							>
-								{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
 								<button className={styles['steps-item-button']}>
 									{i + 1}
 								</button>
-								{/* При клике на кнопку установка выбранного шага в качестве активного */}
 								{step.title}
 							</li>
 						))}
@@ -90,8 +63,6 @@ export const App = () => {
 							className={styles.button}
 						>
 							{lastStep ? 'Начать сначала' : 'Далее'}
-							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-							{/* Или заменять всю кнопку в зависимости от условия */}
 						</button>
 					</div>
 				</div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { App } from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-=======
-import { App } from './App/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> parent of 8f8d012 (components 2)
-=======
-import { App } from './App/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> parent of 8f8d012 (components 2)
 root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 );
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 8f8d012 (components 2)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
->>>>>>> parent of 8f8d012 (components 2)
-=======
->>>>>>> parent of 8f8d012 (components 2)
