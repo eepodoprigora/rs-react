9281c135ba9fe05a412680c5aff924777154d677
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-01 18:00:22 +0300

    Revert "components 2"
    This reverts commit 8f8d012ee414ecf6c503bb65db32eddaeef0ca39.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+<<<<<<< HEAD
 import { App } from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+=======
+import { App } from './App/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+>>>>>>> parent of 8f8d012 (components 2)
 root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 );
+<<<<<<< HEAD
+=======
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+>>>>>>> parent of 8f8d012 (components 2)
