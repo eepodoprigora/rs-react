70bb987edfdadb513eb73ae858771fec8d58cdc5
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-01 18:06:29 +0300

    instruction

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,111 +1,98 @@
 import { useState } from 'react';
 import styles from './app.module.css';
-
-const nums = [
-	{
-		id: '001',
-		value: '1',
-	},
-	{
-		id: '002',
-		value: '2',
-	},
-	{
-		id: '003',
-		value: '3',
-	},
-	{
-		id: '004',
-		value: '4',
-	},
-	{
-		id: '005',
-		value: '5',
-	},
-	{
-		id: '006',
-		value: '6',
-	},
-	{
-		id: '007',
-		value: '7',
-	},
-	{
-		id: '008',
-		value: '8',
-	},
-	{
-		id: '009',
-		value: '9',
-	},
-	{
-		id: '000',
-		value: '0',
-	},
-];
-
-const operators = ['C', '+', '-', '='];
+import data from './data.json';
 
 export const App = () => {
-	const [operand1, setOperand1] = useState('');
-	const [operand2, setOperand2] = useState('');
-	const [operator, setOperator] = useState('');
-	const [done, setDone] = useState(false);
+	// Можно задать 2 состояния — steps и activeIndex
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
+	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	const [firstStep, setFirstStep] = useState(true);
+	const [lastStep, setLastStep] = useState(false);
 
-	function clickNum(num) {
-		if (operator === '') {
-			setOperand1((operand1) => operand1 + num);
-		} else {
-			setOperand2((operand2) => operand2 + num);
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+
+	function nextStep() {
+		setActiveIndex(activeIndex + 1);
+		if (activeIndex + 1 === steps.length - 1) {
+			setLastStep(true);
 		}
-		if (done) setDone(false);
+		if (activeIndex + 1 !== 0) {
+			setFirstStep(false);
+		}
+	}
+	function prevStep() {
+		setActiveIndex(activeIndex - 1);
+		if (activeIndex === 1) {
+			setFirstStep(true);
+		}
 	}
 
-	function clickOperand(item) {
-		if (item === 'C') {
-			setOperand1('');
-			setOperand2('');
-			setOperator('');
-			if (done) setDone(false);
-		} else if (item === '+') {
-			setOperator('+');
-			if (done) setDone(false);
-		} else if (item === '-') {
-			setOperator('-');
-			if (done) setDone(false);
-		} else if (item === '=') {
-			setDone(true);
+	function startOver() {
+		setLastStep(false);
+		setFirstStep(true);
+		setActiveIndex(0);
+	}
+
+	function stepClick(index) {
+		setActiveIndex(index);
+		if (index === steps.length - 1) {
+			setLastStep(true);
+			setFirstStep(false);
+		}
+		if (index === 0) {
+			setLastStep(false);
+			setFirstStep(true);
+		}
+		if (index !== 0 && index !== steps.length - 1) {
+			setLastStep(false);
+			setFirstStep(false);
 		}
 	}
 
 	return (
 		<div className={styles.container}>
-			<div className={styles['inner-container']}>
-				<div className={`${styles.output} ${done ? styles.done : ''}`}>
-					{operand1 + operator + operand2}
-				</div>
-				<div className={styles['buttons-container']}>
-					<div className={styles.buttons}>
-						{nums.map((num) => (
-							<button
-								onClick={() => clickNum(num.value)}
-								className={styles['button-num']}
-								key={num.id}
-							>
-								{num.value}
-							</button>
-						))}
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div key={steps[activeIndex].id} className={styles['steps-content']}>
+						{steps[activeIndex].content}
+						{/* Для получения активного контента использйте steps и activeIndex */}
 					</div>
-					<div className={styles.operators}>
-						{operators.map((operator) => (
-							<button
-								onClick={() => clickOperand(operator)}
-								key={operator}
-								className={`${styles['button-num']} ${styles['button-num--operator']}`}
+
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						{steps.map((step, i) => (
+							<li
+								onClick={() => stepClick(i)}
+								key={step.id}
+								className={`${styles['steps-item']}${activeIndex === i ? ' ' + styles.active : ''}${activeIndex > i ? ' ' + styles.done : ''}`}
 							>
-								{operator}
-							</button>
+								{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
+								<button className={styles['steps-item-button']}>
+									{i + 1}
+								</button>
+								{/* При клике на кнопку установка выбранного шага в качестве активного */}
+								{step.title}
+							</li>
 						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							onClick={prevStep}
+							className={styles.button}
+							disabled={firstStep}
+						>
+							Назад
+						</button>
+						<button
+							onClick={lastStep ? startOver : nextStep}
+							className={styles.button}
+						>
+							{lastStep ? 'Начать сначала' : 'Далее'}
+							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
+							{/* Или заменять всю кнопку в зависимости от условия */}
+						</button>
 					</div>
 				</div>
 			</div>
