daed8219c6e032776e92d0b8e1d2b8efb3f2ca56
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-03 18:18:42 +0300

    json placeholder

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-	return <div className={styles.container}></div>;
+	const [todos, setTodos] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			})
+			.finally(() => setIsLoading(false));
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<h1>Список дел</h1>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				<ul>
+					{todos.map(({ id, title }) => (
+						<li className={styles['list-item']} key={id}>
+							{title}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 };
